a82123fa9060a54cc4bcdaa936649ecc25b7549e
OAtulA <oraonatula@gmail.com>
2022-03-07 13:48:41 +0530

    Better file accessing added

--- notifier.py
+++ notifier.py
@@ -17,32 +17,32 @@ def percent_check():
         percent= percent[:2]
         if charging:
             os.system("notify-send '" +' ' + '\U0001F979' + 'percent%' + "' '" + percent + "'")
-            playsound('~/Music/BatteryNotifySound/mixkit-melodical-flute-music-notification-2310.wav')
+            playsound('Music/BatteryNotifySound/mixkit-melodical-flute-music-notification-2310.wav')
             time.sleep(120)        
         else:
             percent = str(percent)
             percent= percent[:2]
             os.system("notify-send '" +' ' + '\U0001F97A' + 'percent%' + "' '" + percent + "'")
-            playsound('~/Music/BatteryNotifySound/mixkit-wrong-answer-fail-notification-946.wav')
+            playsound('Music/BatteryNotifySound/mixkit-wrong-answer-fail-notification-946.wav')
             time.sleep(180)        
     elif percent >= 30 and percent <= 35:
         percent = str(percent)
         percent= percent[:2]
         os.system("notify-send '" + ' '+ '\U0001F645' + 'percent%' + "' '" + percent + "'")
-        playsound('~/Music/BatteryNotifySound/mixkit-bell-notification-933.wav')
+        playsound('Music/BatteryNotifySound/mixkit-bell-notification-933.wav')
         time.sleep(300)
     elif percent >= 40 and percent <= 45:
         percent = str(percent)
         percent= percent[:2]
         os.system("notify-send '" + ' '+ '\U0001F481' + 'percent%' + "' '" + percent + "'")
-        playsound('~/Music/BatteryNotifySound/mixkit-bell-notification-933.wav')
+        playsound('Music/BatteryNotifySound/mixkit-bell-notification-933.wav')
         time.sleep(300)
     elif percent >=93 and charging:
         percent = str(percent)
         percent= percent[:2]
         os.system("notify-send '"+ ' '+ '\U0001F525' + 'percent%' + "' '" + percent + "'")
-        playsound('~/Music/BatteryNotifySound/mixkit-bell-notification-933.wav')
-        playsound('~/Music/BatteryNotifySound/mixkit-happy-bells-notification-937.wav')
+        playsound('Music/BatteryNotifySound/mixkit-bell-notification-933.wav')
+        playsound('Music/BatteryNotifySound/mixkit-happy-bells-notification-937.wav')
         time.sleep(75)        
     else: # To check every 2 minutes
         time.sleep(120)
