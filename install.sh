7eaace9e72b74c8311422a776c5c3176a0fa6169
OAtulA <oraonatula@gmail.com>
2023-01-01 18:24:38 +0530

    HAPPY NEW YEAR :) 1st commit of 2023

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pip install psutil==5.5.1
 pip install playsound==1.3.0
 
 pip3 install psutil==5.5.1
-pip3 install playsound==1.3.0
+pip3 install playsound==1.3.0  
 
 #To move the sound files to their correct location to be played.
 mkdir ~/Music/BatteryNotifySound
