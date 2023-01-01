224007f08520d2a63685ece320fa3a459d50a11d
OAtulA <oraonatula@gmail.com>
2023-01-01 18:21:24 +0530

    Enhanced the readability of install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#Installing the required python libraries.
 pip install psutil==5.5.1
 pip install playsound==1.3.0
 
 pip3 install psutil==5.5.1
 pip3 install playsound==1.3.0
 
+#To move the sound files to their correct location to be played.
 mkdir ~/Music/BatteryNotifySound
 cp BatteryNotifySound/*.wav ~/Music/BatteryNotifySound
